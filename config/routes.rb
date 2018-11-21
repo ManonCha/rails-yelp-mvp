36e6ceef76bacfd433f5541340b79c09db4a99c2
Manon Charron manoncharron8@gmail.com <manoncharron8@gmail.com>
2018-11-21 15:15:41 +0100

    yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :restaurants, only: [:index, :new, :create, :show]
+  # get 'restaurants', to: 'restaurants#index', as: :restaurants
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_restaurant_review
+  # post 'restaurants/:restaurant_id/reviews', to: 'reviews#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+
+puts 'Creating 10 fake restaurants...'
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::GreekPhilosophers.name,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    address: Faker::Address.street_address,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'
+
